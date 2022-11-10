dd460ef890e6b266ee73e121ce2edace00f0a3f2
epamKbr <117806845+epamKbr@users.noreply.github.com>
2022-11-10 17:10:16 +0530

    Update sumArray.java
    added null exception code

--- day1/sumArray.java
+++ day1/sumArray.java
@@ -1,22 +1,25 @@
 package day1;
 
-public class sumArray {
+public class sumArray  {
 
 	/**
 	* Accepts array of integers and returns sum
 	* @param a - Integer array
 	* @return sum -Sum of elements in array
 	* Time complexity : O(n)
+	 * @throws Exception 
 	*/
-	static int sum(int[] elements)
+	int sum(int[] elements) throws Exception
 	{
+		if (elements == null) throw new NullPointerException("Array is null");
 		int sum = 0;
 		for(int i:elements)
 			sum+=i;
 		return sum;
 	}
-	static int sumRecursive(int[] elements, int xunit)
+	int sumRecursive(int[] elements, int xunit) throws Exception
 	{
+		if (elements == null) throw new NullPointerException("Array is null");
         if (xunit < 0) 
            return 0;
         return sumRecursive(elements, xunit-1 ) + elements[xunit];
