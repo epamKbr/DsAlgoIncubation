9fdffbd4e5af723a1eafb5e433eb5b14cd344f5a
epamKbr <117806845+epamKbr@users.noreply.github.com>
2022-11-10 17:18:21 +0530

    Update sumArrayTest.java
    null unit test added

--- day1/sumArrayTest.java
+++ day1/sumArrayTest.java
@@ -1,28 +1,46 @@
 package day1;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 
 public class sumArrayTest {
 	
-	@Test
-	public void testSum()
+    private static sumArray sumArray;
+    int smallArray[] ={1,-1,2,-2};
+    int nullArray[] =null;
+    
+	@BeforeAll
+	public static void init() 
 	{
-		int a[] = {1,-1,2,-2};
-		int actualSum = sumArray.sum(a);
+	    sumArray = new sumArray();
+    }
+	
+	@Test
+	public void testSum() throws Exception
+	{
+		
+		int actualSum = sumArray.sum(smallArray);
 		int expectedSum = 0;
 		assertEquals(expectedSum, actualSum);
 	}
 	
 	@Test
-	public void testSumRecursive()
+	public void testSumRecursive() throws Exception
 	{
-		int a[] = {1,-1,2,-2};
-		int actualSum = sumArray.sumRecursive(a,a.length-1);
+		int actualSum = sumArray.sumRecursive(smallArray,smallArray.length-1);
 		int expectedSum = 0;
 		assertEquals(expectedSum, actualSum);
 	}
 	
-
+	@Test
+	public void testSumNull() throws Exception
+	{
+		Exception e = assertThrows(NullPointerException.class, () -> sumArray.sum(nullArray));
+		System.out.println(e.getMessage());
+	}
+	
 }
